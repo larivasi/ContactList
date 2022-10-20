6048715cc290901efba2b61937ba336959cc4eca
Vasyl Larin <larinvasyl@protonmail.com>
2022-10-20 12:14:14 +0600

    minor fix

--- ContactList/ViewControllers/ContactCardsViewController.swift
+++ ContactList/ViewControllers/ContactCardsViewController.swift
@@ -23,24 +23,23 @@ class ContactCardsViewController: UITableViewController {
         return 2
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "card", for: indexPath)
-        var content = cell.defaultContentConfiguration()
-        let contact = contactList[indexPath.row]
-        
-        // Так и не понял, что здесь не так. :((
-        switch indexPath.row {
-        case 0:
-            content.image = UIImage(systemName: "phone")
-            content.text = contact.mobile
-        default:
-            content.image = UIImage(systemName: "mail")
-            content.text = contact.email
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "card", for: indexPath)
+            let contact = contactList[indexPath.section]
+            var content = cell.defaultContentConfiguration()
+
+            switch indexPath.row {
+            case 0:
+                content.image = UIImage(systemName: "phone")
+                content.text = contact.mobile
+            default:
+                content.image = UIImage(systemName: "mail")
+                content.text = contact.email
+            }
+    
+            cell.contentConfiguration = content
+            return cell
         }
-        
-        cell.contentConfiguration = content
-        return cell
-    }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         contactList[section].fullName
