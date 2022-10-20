fe467422be6991d6113f5686e62f18c98789fbba
Vasyl Larin <larinvasyl@protonmail.com>
2022-10-20 12:03:36 +0600

    fix code style

--- ContactList/ViewControllers/ContactCardsViewController.swift
+++ ContactList/ViewControllers/ContactCardsViewController.swift
@@ -10,27 +10,25 @@ import UIKit
 class ContactCardsViewController: UITableViewController {
     
     var contactList = Person.getContact()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         contactList.count
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "card", for: indexPath)
         var content = cell.defaultContentConfiguration()
         let contact = contactList[indexPath.row]
         
-        
-// Так и не понял, что здесь не так. :((
+        // Так и не понял, что здесь не так. :((
         switch indexPath.row {
         case 0:
             content.image = UIImage(systemName: "phone")
@@ -39,15 +37,12 @@ class ContactCardsViewController: UITableViewController {
             content.image = UIImage(systemName: "mail")
             content.text = contact.email
         }
-    
         
         cell.contentConfiguration = content
-
         return cell
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         contactList[section].fullName
     }
-
 }
